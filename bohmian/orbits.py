72e653fa2816aafb25823b252fffe26b013ddfd0
senpy <fivoszan@email.com>
2025-05-24 19:29:25 +0300

    adjusting to numiphy

--- bohmian/orbits.py
+++ bohmian/orbits.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
-from numiphy.odesolvers.ode import *
 from numiphy.symlib.symcore import *
 from numiphy.toolkit.plotting import *
+from numiphy.odesolvers import *
 
 
 class VariationalBohmianSystem(OdeSystem):
@@ -19,18 +19,13 @@ class VariationalBohmianSystem(OdeSystem):
             delx_dot = xdot.diff(x)*delx + xdot.diff(y)*dely
             dely_dot = ydot.diff(x)*delx + ydot.diff(y)*dely
         
-
-        renorm = SymbolicPeriodicEvent("Checkpoint", DELTA_T, DELTA_T, [x, y, delx/sqrt(delx**2 + dely**2), dely/sqrt(delx**2 + dely**2)], hide_mask=True)
-
         obj = object.__new__(cls)
         obj.psi = psi
         obj.DELTA_T = DELTA_T
-        return cls._process_args(obj, [xdot, ydot, delx_dot, dely_dot], t, x, y, delx, dely, args=args, events=[renorm])
+        return cls._process_args(obj, [xdot, ydot, delx_dot, dely_dot], t, x, y, delx, dely, args=args)
 
     def get_orbit(self, x0, y0, t0=0., rtol=0., atol=1e-9, min_step=0, max_step=np.inf, first_step=0, args=())->VariationalBohmianOrbit:
-        q0 = np.array([x0, y0, np.sqrt(2)/2, np.sqrt(2)/2])
-        ode = self.get(t0, q0, rtol, atol, min_step, max_step, first_step, args, method="RK45", no_math_errno=True)
-        return VariationalBohmianOrbit(ode)
+        return VariationalBohmianOrbit(f=self.lowlevel_jacobian, t0=t0, q0=[x0, y0, 1, 1], period=self.DELTA_T, rtol=rtol, atol=atol, min_step=min_step, max_step=max_step, first_step=first_step, args=args, events=self.lowlevel_events)
 
     def __eq__(self, other):
         if other is self:
@@ -41,7 +36,7 @@ class VariationalBohmianSystem(OdeSystem):
             return False
 
 
-class BohmianOrbit(Orbit):
+class BohmianOrbit(LowLevelODE):
 
     @property
     def x(self):
@@ -51,18 +46,20 @@ class BohmianOrbit(Orbit):
     def y(self):
         return self.q[:, 1]
     
-    def figure(self, **kwargs):
-        fig = SquareFigure("Bohmian Orbit", xlabel='x', ylabel='y')
-        fig.add(LinePlot(x=self.x, y=self.y, **kwargs))
-        return fig
+    def lineplot(self, **kwargs):
+        return LinePlot(x=self.x, y=self.y, **kwargs)
     
-    def plot(self, **kwargs):
-        return self.figure(**kwargs).plot()
+    def plot(self):
+        fig, ax = plt.subplots()
+        ax.plot(self.x, self.y)
+        return fig, ax
+    
 
 
+class VariationalBohmianOrbit(BohmianOrbit, VariationalLowLevelODE):
 
-class VariationalBohmianOrbit(BohmianOrbit):
-
+    def __init__(self, f: LowLevelJacobian, t0: float, q0: np.ndarray, period: float, *, rtol=1e-6, atol=1e-12, min_step=0., max_step=np.inf, first_step=0., args=(), method="RK45", events: LowLevelEventArray=None, savedir="", save_events_only=False):
+        VariationalLowLevelODE.__init__(self, f, t0, q0, period, rtol=rtol, atol=atol, min_step=min_step, max_step=max_step, first_step=first_step, args=args, method=method, events=events, savedir=savedir, save_events_only=save_events_only)
 
     @property
     def delx(self):
@@ -72,33 +69,6 @@ class VariationalBohmianOrbit(BohmianOrbit):
     def dely(self):
         return self.q[:, 3]
     
-    @property
-    def checkpoints(self):
-        return self.event_map["Checkpoint"]
-
-    @property
-    def logksi(self):
-        ksi_array = np.linalg.norm(self.q[self.checkpoints, 2:], axis=1)
-        logksi = np.cumsum(np.log(ksi_array))
-        return logksi
-    
-    @property
-    def t_lyap(self):
-        return self.t[self.checkpoints]
-    
-    @property
-    def lyap(self):
-        t = self.t_lyap
-        return self.logksi/t
-    
-    def plot_lyap(self, **kwargs):
-        fig = Figure("Lyapunov exponent", xlabel='t', ylabel='$\\lambda$')
-        fig.add(self.lyap_line(**kwargs))
-        return fig.plot()
-    
-    def lyap_line(self, **kwargs):
-        return LinePlot(x=self.t_lyap, y=self.lyap, **kwargs)
-    
 
 
 class OrbitCollection:
@@ -106,9 +76,6 @@ class OrbitCollection:
     def __init__(self, model: VariationalBohmianSystem, ics: Iterable[tuple[float, float]], rtol=0, atol=1e-9, min_step=0, max_step=np.inf, first_step=0, args=()):
         self.model = model
         self.orbits = [self.model.get_orbit(*ic, rtol=rtol, atol=atol, min_step=min_step, max_step=max_step, first_step=first_step, args=args) for ic in ics]
-        self.t_current = 0
-        self._logksi = [[0] for _ in self.orbits]
-        self._t_lyap = [0]
 
     @property
     def DELTA_T(self):
@@ -118,39 +85,19 @@ class OrbitCollection:
     def good_orbits(self):
         return [orb for orb in self.orbits if not orb.is_dead]
     
-    def integrate(self, interval, num, threads=-1, clear=True):
-        goal = self.t_current+interval
-        delta_t = num*self.DELTA_T
-        N = round(interval//delta_t)+1
-        for i in range(N):
-            print(i, '/', N)
-            if not self.good_orbits:
-                break
-            t_int = min(delta_t, goal-self.t_current)
-            self.model.integrate_all(self.good_orbits, interval=t_int, max_frames=0, threads=threads, max_events=-1)
-            self.t_current += t_int
-            if t_int < self.DELTA_T:
-                break
-            self._t_lyap.append(self.t_current)
-            for j, orb in enumerate(self.orbits):
-                if orb.diverges:
-                    continue
-                _logksi = logksi(orb.q[1:])
-                self._logksi[j].append(_logksi[-1]+self._logksi[j][-1])
-                if clear:
-                    orb.clear()
+    def integrate(self, interval, lyap_period, threads=-1):
+        var_integrate_all(self.orbits, interval=interval, lyap_period=lyap_period, threads=threads, max_prints=0)
     
     @property
     def t_lyap(self):
-        return np.array(self._t_lyap[1:])
-
-    @property
-    def logksi(self):
-        return np.array([logksi_[1:] for logksi_, orb in zip(self._logksi, self.orbits) if not orb.is_dead])
+        for orb in self.orbits:
+            if not orb.is_dead:
+                return orb.t_lyap
+        raise ValueError('')
     
     @property
     def lyap_all(self):
-        return self.logksi/self.t_lyap
+        return np.array([orb.lyap for orb in self.good_orbits])
     
     def lyap(self, l=0.005):
         res = self.lyap_all
@@ -172,7 +119,3 @@ class OrbitCollection:
         hist_data, bin_edges = np.histogram(data[:, i], bins, range, density, weights)
         return bin_edges[1:]+np.diff(bin_edges)/2, hist_data
     
-def logksi(q)->np.ndarray:
-    ksi_array = np.linalg.norm(q[:, 2:], axis=1)
-    logksi = np.log(ksi_array).cumsum()
-    return logksi
